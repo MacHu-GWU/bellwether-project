08c9b3c4fd6c57c39ad772d136befa5345eef90a
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-08-09 10:40:38 -0400

    add time utils module

---
+++ bellwether/time_utils.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+
+import typing as T
+from datetime import date, datetime, timedelta, timezone
+from dateutil.tz import UTC
+
+# ------------------------------------------------------------------------------
+# Time integer constants in microseconds
+# ------------------------------------------------------------------------------
+MICRO_SEC = 1
+MILLI_SEC = 1000 * MICRO_SEC
+SEC = 1000 * MILLI_SEC
+
+ONE_SEC = SEC
+FIVE_SEC = 5 * ONE_SEC
+FIFTEEN_SEC = 15 * ONE_SEC
+
+ONE_MINUTE = 60 * ONE_SEC
+FIVE_MINUTE = 5 * ONE_MINUTE
+TEN_MINUTE = 10 * ONE_MINUTE
+ONE_QUARTER = 15 * ONE_MINUTE
+HALF_HOUR = 30 * ONE_MINUTE
+
+ONE_HOUR = 60 * ONE_MINUTE
+TWO_HOUR = 2 * ONE_HOUR
+FOUR_HOUR = 4 * ONE_HOUR
+SIX_HOUR = 6 * ONE_HOUR
+HALF_DAY = 12 * ONE_HOUR
+
+ONE_DAY = 24 * ONE_HOUR
+
+EPOCH = datetime(1970, 1, 1, 0, 0, 0, 0, tzinfo=timezone.utc)
+
+
+def utc_now(delta_seconds: int = 0) -> datetime:
+    """
+    Get the timezone aware UTC now datetime.
+
+    :param delta_seconds: shift the time for x seconds. This parameter can be
+        used to simulate a "historical" or "future" UTC now.
+    """
+    utcnow = datetime.utcnow().replace(tzinfo=timezone.utc)
+    if delta_seconds == 0:
+        return utcnow
+    else:
+        return utcnow + timedelta(seconds=delta_seconds)
+
+
+def ensure_utc_timezone(dt: datetime):
+    """
+    Ensure the datetime object is timezone aware and in UTC timezone
+    """
+    if not (
+        (dt.tzinfo == timezone.utc)
+        or (dt.tzinfo == UTC)
+    ):
+        raise ValueError(f"{dt} is not a timezone aware UTC time")
+
+
+def round_to_begin_of_the_day(dt: datetime) -> datetime:
+    """
+    Round to 00:00:00 AM
+    """
+    return dt.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def round_to_end_of_the_day(dt: datetime) -> datetime:
+    """
+    Round to 23:59:59.999999 PM
+    """
+    return dt.replace(hour=23, minute=59, second=59, microsecond=999999)
+
+
+def round_to_begin_of_the_next_day(dt: datetime) -> datetime:
+    return round_to_begin_of_the_day(dt) + timedelta(days=1)
+
+
+def round_to_end_of_the_previous_day(dt: datetime) -> datetime:
+    return round_to_end_of_the_day(dt) - timedelta(days=1)
+
+
+def date_to_datetime(the_date: date) -> datetime:
+    """
+    Convert date object to UTC aware datetime object.
+    """
+    return datetime(
+        the_date.year,
+        the_date.month,
+        the_date.day,
+        tzinfo=timezone.utc,
+    )
+
+
+def from_utc_timestamp(ts: int) -> datetime:
+    """
+    Assuming the timestamp is between 2000-01-01 to 2100-01-01
+    Smartly convert an integer timestamp to timezone aware UTC time.
+    Automatically detect the time precision, could be second, millisecond,
+    microsecond.
+
+    :param ts: UTC timestamp integer.
+
+    .. note::
+
+        - 2000-01-01 timestamp =   946,684,800
+        - 2022-06-01 timestamp = 1,654,041,600
+        - 2100-01-01 timestamp = 4,102,444,800
+    """
+    ts_2100_1_1 = 4102444800
+    if ts < 0:
+        raise ValueError
+    elif ts <= ts_2100_1_1:
+        return datetime.utcfromtimestamp(ts).replace(tzinfo=timezone.utc)
+    elif ts <= ts_2100_1_1 * 1000:
+        return datetime.utcfromtimestamp(ts / 1000).replace(tzinfo=timezone.utc)
+    elif ts <= ts_2100_1_1 * 1000000:
+        return datetime.utcfromtimestamp(ts / 1000000).replace(tzinfo=timezone.utc)
+    else:
+        raise ValueError
+
+
+def to_timestamp_in_seconds(dt: datetime) -> int:
+    """
+    Convert datetime object to its timestamp in seconds,
+    ignore the milliseconds and
+    microseconds part.
+    """
+    return int(dt.timestamp())
+
+
+def to_timestamp_in_milliseconds(dt: datetime) -> int:
+    """
+    Convert datetime object to its timestamp in milliseconds,
+    ignore the microseconds part.
+    """
+    return int(dt.timestamp() * 1000)
+
+
+def to_timestamp_in_microseconds(dt: datetime) -> int:
+    """
+    Convert datetime object to its timestamp in microseconds
+    """
+    return int(dt.timestamp() * 1000000)
---
+++ tests/test_time_utils.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pytest
+from datetime import datetime, date, timezone
+from bellwether.time_utils import (
+    utc_now,
+    ensure_utc_timezone,
+    round_to_begin_of_the_day,
+    round_to_end_of_the_day,
+    round_to_begin_of_the_next_day,
+    round_to_end_of_the_previous_day,
+    date_to_datetime,
+    from_utc_timestamp,
+    to_timestamp_in_seconds,
+    to_timestamp_in_milliseconds,
+    to_timestamp_in_microseconds,
+)
+
+
+def test_utc_now():
+    now = utc_now()
+    assert now.tzinfo == timezone.utc
+
+    # test delta_seconds parameter
+    now1 = utc_now()
+    now2 = utc_now(delta_seconds=-10)
+    now3 = utc_now(delta_seconds=10)
+    assert (now1 - now2).total_seconds() == pytest.approx(10, 0.1)
+    assert (now3 - now1).total_seconds() == pytest.approx(10, 0.1)
+
+
+def test_ensure_utc_timezone():
+    with pytest.raises(ValueError):
+        ensure_utc_timezone(datetime.now())
+
+    ensure_utc_timezone(utc_now())
+
+
+def test_round_to_begin_of_the_day():
+    assert round_to_begin_of_the_day(datetime(2000, 1, 5, 8)) == datetime(2000, 1, 5)
+
+
+def test_round_to_end_of_the_day():
+    assert round_to_end_of_the_day(datetime(2000, 1, 5, 8)) == datetime(
+        2000, 1, 5, 23, 59, 59, 999999
+    )
+
+
+def test_round_to_begin_of_the_next_day():
+    assert round_to_begin_of_the_next_day(datetime(2000, 1, 5, 8)) == datetime(
+        2000, 1, 6
+    )
+
+
+def test_round_to_end_of_the_previous_day():
+    assert round_to_end_of_the_previous_day(datetime(2000, 1, 5, 8)) == datetime(
+        2000, 1, 4, 23, 59, 59, 999999
+    )
+
+
+def test_date_to_datetime():
+    assert date_to_datetime(date(2000, 1, 1)) == datetime(2000, 1, 1, tzinfo=timezone.utc)
+
+
+def test_from_utc_timestamp():
+    dt = from_utc_timestamp(1654041600)
+    assert str(dt) == "2022-06-01 00:00:00+00:00"
+
+    dt = from_utc_timestamp(0)
+    assert str(dt) == "1970-01-01 00:00:00+00:00"
+
+    dt = from_utc_timestamp(1654041600123)
+    assert str(dt) == "2022-06-01 00:00:00.123000+00:00"
+
+    dt = from_utc_timestamp(1654041600123456)
+    assert str(dt) == "2022-06-01 00:00:00.123456+00:00"
+
+    with pytest.raises(ValueError):
+        from_utc_timestamp(-1)
+
+    with pytest.raises(ValueError):
+        from_utc_timestamp(999999999999999999)
+
+
+def test_to_timestamp_in_seconds():
+    dt = datetime(1970, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
+    assert to_timestamp_in_seconds(dt) == 0
+
+    dt = datetime(1970, 1, 1, 0, 0, 0, 123000, tzinfo=timezone.utc)
+    assert to_timestamp_in_seconds(dt) == 0
+
+    dt = datetime(1970, 1, 1, 0, 0, 0, 999999, tzinfo=timezone.utc)
+    assert to_timestamp_in_seconds(dt) == 0
+
+
+def test_to_timestamp_in_milliseconds():
+    dt = datetime(1970, 1, 1, 0, 0, 1, tzinfo=timezone.utc)
+    assert to_timestamp_in_milliseconds(dt) == 1000
+
+    dt = datetime(1970, 1, 1, 0, 0, 1, 123000, tzinfo=timezone.utc)
+    assert to_timestamp_in_milliseconds(dt) == 1123
+
+    dt = datetime(1970, 1, 1, 0, 0, 1, 123456, tzinfo=timezone.utc)
+    assert to_timestamp_in_milliseconds(dt) == 1123
+
+    dt = datetime(1970, 1, 1, 0, 0, 1, 123999, tzinfo=timezone.utc)
+    assert to_timestamp_in_milliseconds(dt) == 1123
+
+
+def test_to_timestamp_in_microseconds():
+    dt = datetime(1970, 1, 1, 0, 0, 1, 0, tzinfo=timezone.utc)
+    assert to_timestamp_in_microseconds(dt) == 1000000
+
+    dt = datetime(1970, 1, 1, 0, 0, 1, 123, tzinfo=timezone.utc)
+    assert to_timestamp_in_microseconds(dt) == 1000123
+
+    dt = datetime(1970, 1, 1, 0, 0, 1, 999999, tzinfo=timezone.utc)
+    assert to_timestamp_in_microseconds(dt) == 1999999
+
+    dt = datetime(2022, 6, 1, 0, 0, 0, 0, tzinfo=timezone.utc)
+    assert to_timestamp_in_microseconds(dt) == 1654041600000000
+
+
+if __name__ == "__main__":
+    import sys
+    import subprocess
+
+    abspath = os.path.abspath(__file__)
+    dir_project_root = os.path.dirname(abspath)
+    for _ in range(10):
+        if os.path.exists(os.path.join(dir_project_root, ".git")):
+            break
+        else:
+            dir_project_root = os.path.dirname(dir_project_root)
+    else:
+        raise FileNotFoundError("cannot find project root dir!")
+    dir_htmlcov = os.path.join(dir_project_root, "htmlcov")
+    bin_pytest = os.path.join(os.path.dirname(sys.executable), "pytest")
+
+    args = [
+        bin_pytest,
+        "-s", "--tb=native",
+        f"--rootdir={dir_project_root}",
+        "--cov=bellwether.time_utils",
+        "--cov-report", "term-missing",
+        "--cov-report", f"html:{dir_htmlcov}",
+        abspath,
+    ]
+    subprocess.run(args, check=True)
